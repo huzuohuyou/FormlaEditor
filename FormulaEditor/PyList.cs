859782ffe653eb4de1c800f9ed945a81095d7d78
hailong wu <13126506430@163.com>
2017-05-26 15:22:36 +0800

    1.oop优化

---
+++ FormulaEditor/PyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormulaEditor
+{
+    public class PyList
+    {
+        public List<PyFile> PList = new List<PyFile>();
+        public PyList() {
+            string pythonDir = string.Format("{0}\\PythonFiles", Application.StartupPath);
+            if (!Directory.Exists(pythonDir))
+            {
+                Directory.CreateDirectory(pythonDir);
+            }
+            else
+            {
+                DirectoryInfo di = new DirectoryInfo(pythonDir);
+                FileInfo[] fi = di.GetFiles();
+                foreach (var item in fi)
+                {
+                    PyFile pf = new PyFile() {Name=item.Name,Path = item.FullName,Content = File.ReadAllText(item.FullName) };
+                    PList.Add(pf);
+                }
+            }
+        }
+    }
+}
--- FormulaEditor/PythonFiles/demo.py
+++ FormulaEditor/PythonFiles/demo.py
@@ -4,3 +4,33 @@
 
 def plus(p1,p2):
 	return p1+p2
+
+def sub(p1,p2):
+	return p1-p2
+
+def multi(p1,p2):
+	return p1*p2
+
+def div(p1,p2):
+	return p1/p2
+
+def test(p1,p2,p3,p4,p5):
+	if(p5>0):
+		if(p3>p4):
+			return -1
+		else:
+			return -2
+	else:
+		return -3
+
+def methon_factory(methon_name,p1=None,p2=None,p3=None,p4=None,p5=None):
+	if(methon_name=="plus"):
+		return plus(p1,p2)
+	elif(methon_name=="sub"):
+		return sub(p1,p2)
+	elif(methon_name=="multi"):
+		return multi(p1,p2)
+	elif(methon_name=="div"):
+		return div(p1,p2)
+	elif(methon_name=="test"):
+		return test(p1,p2,p3,p4,p5)
