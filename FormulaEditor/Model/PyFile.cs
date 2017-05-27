c8ec9901031b42aaceaa3d3d27069cb51b4442f0
hailong wu <13126506430@163.com>
2017-05-27 16:56:44 +0800

    1.修复模板初始化错误bug;

--- FormulaEditor/Model/PyFile.cs
+++ FormulaEditor/Model/PyFile.cs
@@ -90,7 +90,7 @@ def method_factory(method_name,p1=None,p2=None,p3=None,p4=None,p5=None,p6=None,p
 	return func(p1,p2,p3,p4,p5,p6,p7,p8)
 class Foo:
 	def demo(self,p1=None,p2=None,p3=None,p4=None,p5=None,p6=None,p7=None,p8=None):
-		return ’测试方法‘";
+		return '测试方法'";
         }
 
         
